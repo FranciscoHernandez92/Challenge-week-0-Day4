b5b1b9710d544dd175f3fdef9fd4368b092bfd2c
fran hernandez <franciscohernandezlopez5@gmail.com>
2024-02-15 17:25:01 +0100

    First ex

---
+++ index.js
@@ -0,0 +1,4 @@
+//1. 
+import { deleter } from "./module.js"
+console.log(deleter('palmera'))
+//2.
---
+++ module.js
@@ -0,0 +1,5 @@
+// 1.
+export const deleter = (str) =>{
+  const string = str.slice(1, -1)
+  return string
+}
