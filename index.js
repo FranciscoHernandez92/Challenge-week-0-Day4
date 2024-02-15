faeaccdfa4fc45732f00d99dcfaa3fcd69b0fb41
fran hernandez <franciscohernandezlopez5@gmail.com>
2024-02-15 18:13:26 +0100

    Add second and third ex

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 import { deleter } from "./module.js"
 console.log(deleter('palmera'))
 //2.
+import { palin } from "./module.js"
+console.log(palin('paco'));
+//3.
+import { vocals } from "./module.js";
+console.log(vocals('santana'));
--- module.js
+++ module.js
@@ -3,3 +3,18 @@ export const deleter = (str) =>{
   const string = str.slice(1, -1)
   return string
 }
+// 2. 
+export const palin = (r) =>{
+  const word = r.split('').reverse('').join('')
+  return word === r
+}
+//3. 
+export const vocals = (w) => {
+  let sum = 0
+  for (let i = 0; i < w.length; i++){
+   if(w[i] === 'a' || w[i] === 'e' || w[i] === 'i' || w[i] === 'o' || w[i] === 'u'){
+    sum++
+   }
+  } return sum 
+} 
+
