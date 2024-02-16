e1ecab61692c0d06cec5cbfb21a3ed3c77745c81
fran hernandez <franciscohernandezlopez5@gmail.com>
2024-02-16 12:32:16 +0100

    Add more excer

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 //1. 
 import { deleter } from "./module.js"
 console.log(deleter('palmera'))
+console.log(deleter('un'));
 //2.
-import { palin } from "./module.js"
-console.log(palin('paco'));
-console.log(palin('oso'));
+import { isPalin } from "./module.js"
+console.log(isPalin('paco'));
+console.log(isPalin('oso'));
+console.log(isPalin('Oso'));
 //3.
 import { vocals } from "./module.js";
 console.log(vocals('santana'));
 console.log(vocals('cartel'));
 //4
-
+import { isPangram } from "./module.js";
+console.log(isPangram('La vuelta al mundo en 80 dias'))
+console.log(isPangram('Un jugoso zumo de piña y kiwi bien frío es exquisito y no lleva alcohol.'));
 //5. 
 import { allVocals } from "./module.js";
 console.log(allVocals('paco'));
@@ -20,11 +24,16 @@ import { countDown } from "./module.js";
 console.log(countDown(15));
 console.log(countDown(7));
 //7.
-
+import { calculateAge } from "./module.js";
+console.log(calculateAge(1992));
 //8.
 import { major } from "./module.js";
 console.log(major(12));
 console.log(major(24));
+//Otra manera para calcular segun el año de nacimiento si eres mayor de edad
+import { isAdult } from "./module.js";
+console.log(isAdult('1998-12-31'));
+console.log(isAdult('2010-12-31'));
 //9.
 import { random } from "./module.js";
 console.log(random());
--- module.js
+++ module.js
@@ -1,12 +1,16 @@
 // 1.
 export const deleter = (str) =>{
+  if(str.length < 3){
+    return ' imposible remove letters.'
+  }
   const string = str.slice(1, -1)
   return string
 }
 // 2. 
-export const palin = (r) =>{
+export const isPalin = (r) =>{
+  // tambien se puede crear una constante directamente: const lowerCase = r.toLowerCaser()
   const word = r.split('').reverse('').join('')
-  return word === r
+  return word.toLowerCase() === r.toLowerCase()
 }
 //3. 
 export const vocals = (w) => {
@@ -17,8 +21,27 @@ export const vocals = (w) => {
    }
   } return sum 
 } 
+// Otra forma
+const countVocals = (inputWord) => {
+  const lowerWord = inputWord.toLowerCase()
+  const vowels =  ['a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ú', 'ü']
+  let counter = 0 
+  for (let i = 0; i < lowerWord.length; i++ ){
+    if (vowels.includes(lowerWord[i])){
+      counter++  
+    }
+  } return counter
+}
 //4.
-
+export const isPangram = (inputString) => {
+  const lowerString = inputString.toLowerCase()
+  const alphabet = ['abcdefghijklmnopqrstuvwxyz']
+  for (let i = 0; i < alphabet.length; i++){
+    if (!lowerString.includes(alphabet[i])){
+      return false
+    }
+  } return true 
+}
 //5.
 export const allVocals = (v) => {
   return v.includes('a' && 'e' && 'i' && 'o' && 'u') 
@@ -32,12 +55,26 @@ export const countDown = (d) => {
   } return count.join(', ')
   }
 //7.
-
+export const calculateAge = (year) =>{
+  const now = new Date() //esto lo que hace es que te da el valor de la fecha actual.
+  const currentYear = now.getFullYear() //jugar con la propiedad .get... e indicar la funcion que te interesa.
+  return  currentYear - year
+}
 //8.
 export const major = (o) => {
   if(o >= 18){
     return 'you can go to jail'
   }return 'you are a teenager'
+}
+//Otra forma para calcular segun el año de nacimiento si eres mayor de edad
+export const isAdult = (dateIso) =>{
+  const birthDate = new Date(dateIso)
+  const now = new Date()
+  const currentYear = now.getFullYear()
+  const age = currentYear - birthDate.getFullYear()
+  if( age >= 18){
+    return 'estas cascao'
+  } return 'eres un pipiolo'
 }
 //9.
 export const random = () => {
@@ -51,20 +88,6 @@ for (let i = 0; i < 7; i++) {
 //10.
 
 //11.
-const game1 = (g, c) => {
-  const r = 'rock'
-  const p = 'paper'
-  const s = 'scissors'
-  let q = random(r, p, s)
-  if((g === r ) && (c === s) || (g === p) && (c === r) || (g === s) && (c === p)){
-    return `${g} v ${c} you win` 
-  } return `${g} v ${c} sorry, try again`
-  function random(...options) {
-    const index = Math.floor(Math.random() * options.length)
-    return options[index]
-  }
-}
-
 export const game = (play) =>{
   const myOptions = ['piedra', 'papel', 'tijera']
   const computerOption = myOptions[Math.floor(Math.random() * myOptions.length)]
